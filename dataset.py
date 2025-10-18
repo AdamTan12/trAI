5d51421b39429e6194596f6d64103a5c4eeea6f2
XxKillerDemonxX <149117791+AdamTan12@users.noreply.github.com>
2025-10-17 21:18:46 -0700

    added some documentation

--- dataset.py
+++ dataset.py
@@ -4,13 +4,27 @@ from torch.utils.data import Dataset, dataloader
 from torchvision.transforms import functional as TF
 from PIL import Image
 
+
 class bbox_dataset(Dataset):
+    """
+    creates a Dataset of images and uses four point bounding boxes as labels
+
+    Attributes:
+        img_dir (list): List of image paths
+        img_labels (list): List of bounding box labels
+    """
     def __init__(self, annotations, images):
         # Initialize lists
         self.img_dir = [images[annotation["image_id"]]["file_name"] for annotation in annotations]
         self.img_labels = [annotation["bbox"] for annotation in annotations]
+    """
+    returns length of dataset
+    """
     def __len__(self):
         return len(self.img_labels)
+    """
+    returns the tensors of an image and labels at the index
+    """
     def __getitem__(self, idx):
         img_path = self.img_dir[idx]
         image = Image.open(img_path).convert("RGB")
@@ -21,14 +35,27 @@ class bbox_dataset(Dataset):
         return image_tensor, label_tensor
 
 class category_dataset(Dataset):
+    """
+    creates a Dataset of images and uses categories as labels
+
+    Attributes:
+        img_dir (list): List of image paths
+        img_labels (list): List of category labels as one hot encodings
+    """
     def __init__(self, annotations, categories, images):
         # initialize lists
         self.img_dir = [images[annotation["image_id"]]["file_name"] for annotation in annotations]
         # one hot encoding
         # convert category_id into a 1x1 tensor
         self.img_labels = [F.one_hot(torch.tensor(annotation["category_id"], dtype=torch.long), num_classes=len(categories)).squeeze(0).float() for annotation in annotations]
+    """
+    return length of dataset
+    """
     def __len__(self):
         return len(self.img_labels)
+    """
+    returns the tensors of an image and labels at the index
+    """   
     def __getitem__(self, idx):
         img_path = self.img_dir[idx]
         image = Image.open(img_path).convert("RGB")
